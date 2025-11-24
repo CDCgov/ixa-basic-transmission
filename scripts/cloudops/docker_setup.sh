39febbcb7031ef4e343e8182fa1ec5864d3c4154
Gil Sharon <as81@cdc.gov>
2025-11-24 11:30:41 -0500

    test action

---
+++ scripts/cloudops/docker_setup.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+apt-get update && apt-get install -y curl unzip ca-certificates
+
+echo "Downloading latest ixa-basic-transmission code..."
+curl -L "https://github.com/CDCgov/ixa-basic-transmission/archive/refs/heads/main.zip" -o code.zip
+
+echo "Unzipping..."
+unzip -o code.zip
+rm code.zip
+
+mv ixa-basic-transmission-main ixa-basic-transmission
+cd ixa-basic-transmission
+
+curl https://raw.githubusercontent.com/CDCgov/ocio-certificates/refs/heads/main/data/min-cdc-bundle-ca.crt | tee /usr/local/share/ca-certificates/min-cdc-bundle-ca.crt >/dev/null
+update-ca-certificates
+
+cargo run -- --params params/default.toml
